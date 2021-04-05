cd10a3e51fcb2d425be2bd7e005833108e5d02fa
irohitb <irohitbhatia@gmail.com>
2021-04-05 17:51:27 +0530

    feat: inital commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { connect } from 'react-redux';
+
+
+export const Routes = () => {
+    return (
+        <NavigationContainer>
+
+        </NavigationContainer>
+    )
+
+}
+
+const mapStateToProps = (state: any) => state
+
+  
+export default connect(mapStateToProps, {})(Routes);
